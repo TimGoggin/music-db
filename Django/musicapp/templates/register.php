c6db89c971d380ae63adb834c42c2ef66fa5fe72
Nathan Hausspiegel <nathanhausspiegel@Nathans-MacBook-Pro-2.local>
2022-03-09 17:11:54 -0500

    Updated CSS in Django files (may not work)

--- Django/musicapp/templates/register.php
+++ Django/musicapp/templates/register.php
@@ -1,8 +1,3 @@
-<!-- 
-    COMP 333: Software Engineering
-    Nathan Hausspiegel
--->
-
 <!DOCTYPE HTML>
 
 <html lang="en">
@@ -10,29 +5,35 @@
 <head>
 
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
 
     <title>music-db</title>
-    <h1>music-db</h1>
 
 </head>
- 
-<body>
-    <h2>Registration</h2>
 
-    <form method="POST">
-    {% csrf_token %}
-    {{ form.username.label }} {{ form.username }}<br>
-    {{ form.password.label }} {{ form.password }}<br>
-    <input type="submit" name="register" value="Register"/>
-    </form>
+    <div class="content">
 
-    <!-- 
-        Make sure that there is a value available for $ratings_out.
-        If so, print to the screen.
-    -->
-    <p>{{submittext}}</p>
-    <br>
-    <a href="index.html">Home</a>
+        <h1>music-db</h1>
+
+        <h2>Registration</h2>
+
+        <form method="POST">
+        {% csrf_token %}
+        {{ form.username.label }} {{ form.username }}<br>
+        {{ form.password.label }} {{ form.password }}<br>
+        <input type="submit" name="register" value="Register"/>
+        </form>
+
+        <!-- 
+            Make sure that there is a value available for $ratings_out.
+            If so, print to the screen.
+        -->
+        <p>{{submittext}}</p>
+        <br>
+        <a href="index.html">Home</a>
+
+    </div>
+    
 </body>
 
 </html>
