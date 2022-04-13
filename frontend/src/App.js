f4564fad8cc345947f876f82a8918646d37e96eb
TimGoggin <tgoggin@wesleyan.edu>
2022-04-13 12:46:03 -0400

    site can now play one song

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import React from 'react';
 import Song_lookup from './components/song_lookup';
+import Play from './components/play.js';
 
 function App() {
   return (
     <div className="App">
       <Song_lookup />
+      < Play />
     </div>
   );
 
